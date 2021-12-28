9e89d203a6a96bab15d171c839459d9efdbe50c4
@skylerbair <skylertimothybair@hotmail.com>
2021-12-28 13:53:50 -0700

    created repeat test for iteration example

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -2,6 +2,14 @@ package iteration
 
 import "testing"
 
+func Repeat(character string) string {
+	var repeated string
+	for i := 0; i < 5; i++ {
+		repeated = repeated + character
+	}
+	return repeated
+}
+
 func TestRepeat(t *testing.T) {
 	repeated := Repeat("a")
 	expected := "aaaaa"
