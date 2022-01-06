066297142a4e30793686d9e01b16b186d6d27d26
@skylerbair <skylertimothybair@hotmail.com>
2022-01-06 13:06:25 -0700

    finished struct, method and interface examples

--- pointers-errors/wallet.go
+++ pointers-errors/wallet.go
@@ -14,26 +14,6 @@ type Stringer interface {
 	String() string
 }
 
-assertError := func(t testing.TB, got error, want string) {
-	t.Helper()
-	if err == nil {
-		t.Fatal("Didnt get a error but wanted one")
-	}
-
-	if got.Error() != want {
-		t.Errorf("got %q, want %q", got, want)
-	}
-}
-
-t.Run("Withdraw insufficent funds", func(t *testing.T) {
-	startingBalance := Bitcoin(20)
-	wallet := Wallet{startingBalance}
-	err := wallet.Withdraw(Bitcoin(100))
-
-	assertError(t, err, "cannot withdraw, insufficient funds")
-	assertBalance(t, wallet, startingBalance)
-})
-
 func (b Bitcoin) String() string {
 	return fmt.Sprintf("%d BTC", b)
 }
@@ -48,13 +28,14 @@ func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
 
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
 func (w *Wallet) Withdraw(amount Bitcoin) error {
 
 	if amount > w.balance {
-		return errors.New("oh no")
+		return ErrInsufficientFunds
 	}
+
 	w.balance -= amount
 	return nil
 }
-
-
